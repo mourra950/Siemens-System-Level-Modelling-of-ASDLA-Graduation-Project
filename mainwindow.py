8e8dd97d113dee52afa0d20bb482d6dba0bb7682
Omar Mohamed Yousef <mourra24@gmail.com>
2023-11-19 23:43:51 +0200

    Minor updates to the window title

--- mainwindow.py
+++ mainwindow.py
@@ -5,7 +5,9 @@ from PySide6.QtUiTools import QUiLoader
 basedir = os.path.dirname(__file__)
 loader = QUiLoader()
 app = QtWidgets.QApplication(sys.argv)
+
 window = loader.load(os.path.join(basedir, "mainwindow.ui"), None)
+window.setWindowTitle("System Level Modelling")
 with open("skin.qss", "r") as f:
         _style = f.read()
         app.setStyleSheet(_style)
