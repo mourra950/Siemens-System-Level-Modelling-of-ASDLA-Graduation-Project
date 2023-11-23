7d2c4fef08656abe4ce9f8f53459f3db2515cfa6
OmarElbanna <elbanna16@gmail.com>
2023-11-23 23:02:14 +0200

    Added an example for file and directory picker

---
+++ examples/qt_file_picker/main.py
@@ -0,0 +1,55 @@
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QFileDialog
+from PySide6.QtUiTools import QUiLoader
+from PySide6 import QtCore
+import os
+import sys
+from pathlib import Path
+
+basedir = os.path.dirname(__file__)
+loader = QUiLoader()
+
+class MainUI(QtCore.QObject):
+    def __init__(self):
+        super().__init__()
+        self.path=""
+        self.ui = loader.load(
+        os.path.join(basedir, "MainWindow.ui"), None
+        )
+        self.ui.setWindowTitle("Testing File Picker")
+
+        self.pushButton1 = self.ui.findChild(QPushButton, 'pushButton1')
+        self.pushButton2 = self.ui.findChild(QPushButton, 'pushButton2')
+        self.label1 = self.ui.findChild(QLabel, 'label1')
+
+        self.pushButton1.clicked.connect(self.getFile)
+        self.pushButton2.clicked.connect(self.getDirectory)
+
+
+        self.ui.show()
+
+
+
+    def getFile(self):
+        path,_ = QFileDialog.getOpenFileName(None,'Open file',basedir,"JSON Files (*.json)")
+        self.path = Path(path)
+        print(self.path)
+        self.label1.setText(str(self.path))
+
+
+
+    def getDirectory(self):
+        dir_name = Path(QFileDialog.getExistingDirectory(None, "Select a Directory"))
+        print(dir_name)
+        self.path = dir_name
+        self.label1.setText(str(self.path))
+
+
+def main():
+  
+   app = QApplication(sys.argv)
+   window = MainUI()
+   app.exec()
+
+
+if __name__ == '__main__':
+    main()
