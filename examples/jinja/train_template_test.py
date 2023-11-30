431cd7c4141e731961dbfb565f2b3e6cb7f86817
Adham-Abdelmaksoud <adham.5050@hotmail.com>
2023-11-30 20:55:05 +0200

    Created a function to render any jinja template into python code

--- examples/jinja/train_template_test.py
+++ examples/jinja/train_template_test.py
@@ -1,33 +1,11 @@
-from jinja2 import Environment, FileSystemLoader
-import os
+from TemplateRenderer import convert_jinja_to_code
 
-# define model hyperparameters
-parameters = {
+# This is just a test file to demonistrate the template rendering
+# and is to be later deleted
+convert_jinja_to_code('templates/train.py.jinja', 'output/train.py', {
     'learning_rate': 0.01,
     'batch_size': 256,
     'epochs': 1,
     'optimizer': 'Adadelta',
     'loss_func': 'L1Loss'
-}
-
-# define the template path
-base_dir = os.path.dirname(os.path.abspath(__file__))
-template_dir = os.path.join(base_dir, 'templates')
-template_filename = 'train.py.jinja'
-
-# define the environment and get the template
-env = Environment(loader=FileSystemLoader(template_dir))
-template = env.get_template(template_filename)
-
-# render the template using the defined parameters
-train_code = template.render(parameters)
-print(train_code)
-
-# define the output path
-output_dir = os.path.join(base_dir, 'output')
-output_filename = 'train.py'
-output_path = os.path.join(output_dir, output_filename)
-
-# save the rendered template as a .py file
-with open(output_path, 'w') as f:
-    f.write(train_code)
+})
