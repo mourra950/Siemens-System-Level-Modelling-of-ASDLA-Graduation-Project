d9c7307932b4db31261cc42ed32720bdd8f2bba9
Omar Mohamed Yousef <mourra24@gmail.com>
2023-12-02 00:16:46 +0200

    Successfully been able to iterate over pytorch library
    took the first steps in automating the flow of extracting params and args to automate the extraction and creation of the configurations file needed for the project

--- examples/auto_extraction/test1.py
+++ examples/auto_extraction/test1.py
@@ -6,19 +6,27 @@ from torch.nn import Conv1d
 
 def print_dataset_arguments(dataset_class):
     print(f"{dataset_class.__name__} dataset arguments:")
-    for name, parameter in inspect.signature(dataset_class.__init__).parameters.items():
+    for name, parameter in inspect.getmembers(object=dataset_class):
         if name != 'self':
-            print(f"{name}: {parameter.annotation}")
+            print(f"{name}: {parameter}" )
 
 # Example: CIFAR10
-print_dataset_arguments(CIFAR10)
+# print_dataset_arguments(CIFAR10)
 print("===================================")
+for i in MNIST.__dict__:
+    print(i)
+    print(MNIST.__dict__[i])
+print("===================================")
+
+for name, parameter in inspect.signature(MNIST.__init__).parameters.items():
+    print(name)
 
 # Example: MNIST
-print_dataset_arguments(MNIST)
+# print_dataset_arguments(MNIST)
 print("===================================")
 
+print(MNIST.__dict__['__parameters__'])
 # Example: Convulation
 
-print_dataset_arguments(Conv1d)
+# print_dataset_arguments(Conv1d)
 print("===================================")
