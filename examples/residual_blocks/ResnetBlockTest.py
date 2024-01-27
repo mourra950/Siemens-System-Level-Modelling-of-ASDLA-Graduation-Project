2b82bd7a24caba7371ea2188507fef2efbc1939e
OmarElbanna <elbanna16@gmail.com>
2024-01-27 21:31:23 +0200

    Updated residualBlock jinja template

---
+++ examples/residual_blocks/ResnetBlockTest.py
@@ -0,0 +1,66 @@
+from util.TemplateRenderer import convertJinjaToCode
+
+layers = [
+    {'name': 'conv1', 'type': 'Conv2d', 'params': {'in_channels': 64, 'out_channels': 128, 'kernel_size': 1, 'stride': 1, 'padding': 0}},
+    {'name': 'bn1', 'type': 'BatchNorm2d', 'params': {'num_features': 128}},
+    {'name':'relu1','type':'ReLU'}
+]
+
+layers1 = [
+    {
+        'name': 'conv1',
+        'type': 'Conv2d',
+        'params': {
+            'kernel_size': 1,
+            'stride': 1,
+            'padding': 0
+        }
+    },
+    {
+        'name': 'bn1',
+        'type': 'BatchNorm2d',
+        'params': {}
+    },
+    
+    {
+        'name': 'conv2',
+        'type': 'Conv2d',
+        'params': {
+            'kernel_size': 3,
+            'stride': 1,
+            'padding': 1
+        }
+    },
+    {
+        'name': 'bn2',
+        'type': 'BatchNorm2d',
+        'params': {}
+    },
+
+    {
+        'name': 'conv3',
+        'type': 'Conv2d',
+        'params': {
+            'kernel_size': 1,
+            'stride': 1,
+            'padding': 0
+        }
+    },
+    {
+        'name': 'bn3',
+        'type': 'BatchNorm2d',
+        'params': {}
+    },
+
+    {
+        'name': 'relu',
+        'type': 'ReLU',
+        'params': {}
+    },
+]
+
+convertJinjaToCode('templates/residualBlock1.py.jinja', 'output/residual.py',
+    {
+        'layers': layers1
+    }                   
+)
