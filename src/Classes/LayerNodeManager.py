4a90a8cb6b93bccca33c5ab09e0c823bb69c32fa
OmarElbanna <elbanna16@gmail.com>
2024-03-19 00:57:44 +0200

    Solved JSON Files Write Issue
    Parametrized the functions that modify the architecture dictionary with the dict we want to modify

--- src/Classes/LayerNodeManager.py
+++ src/Classes/LayerNodeManager.py
@@ -18,8 +18,11 @@ class LayerNodeManager:
                 'channels': 1,
             }
         }
+        self.Resarchitecture = {
+            'layers': []
+        }
 
-    def create_layer_node(self, layer, index, qt_layout):
+    def create_layer_node(self, layer, index, qt_layout, arch_dict):
         addedLayerRow_QHBoxLayout = QHBoxLayout()
         border_QFrame = QFrame()
         border_QFrame.setMaximumHeight(70)
@@ -35,11 +38,11 @@ class LayerNodeManager:
             }
         ''')
         delete_QPushButton = self.qbutton_layer_manager(
-            self.delete_icon_path, qt_layout, border_QFrame, self.on_delete_layer_clicked)
+            self.delete_icon_path, qt_layout, border_QFrame, self.on_delete_layer_clicked,arch_dict)
         up_QPushButton = self.qbutton_layer_manager(
-            self.up_icon_path, qt_layout, border_QFrame, self.on_move_buttons_clicked, "up")
+            self.up_icon_path, qt_layout, border_QFrame, self.on_move_buttons_clicked, "up",arch_dict)
         down_QPushButton = self.qbutton_layer_manager(
-            self.down_icon_path, qt_layout, border_QFrame, self.on_move_buttons_clicked,"down")
+            self.down_icon_path, qt_layout, border_QFrame, self.on_move_buttons_clicked,"down",arch_dict)
 
         moveableArrows_QVBoxLayout = QVBoxLayout()
         moveableArrows_QVBoxLayout.addWidget(up_QPushButton)
@@ -51,24 +54,24 @@ class LayerNodeManager:
 
         if index == -1:
             qt_layout.addWidget(border_QFrame)
-            self.architecture['layers'].append(layer)
+            arch_dict['layers'].append(layer)
         else:
             qt_layout.insertWidget(index, border_QFrame)
-            self.architecture['layers'].insert(index, layer)
+            arch_dict['layers'].insert(index, layer)
 
-    def on_delete_layer_clicked(self, border_QFrame, qt_layout,*args):
-        for i in range(len(self.architecture['layers'])):
+    def on_delete_layer_clicked(self, border_QFrame, qt_layout,arch_dict):
+        for i in range(len(arch_dict['layers'])):
             if border_QFrame == qt_layout.itemAt(i).widget():
                 layer_widget = qt_layout.itemAt(i).widget()
 
-                self.architecture['layers'].pop(i)
+                arch_dict['layers'].pop(i)
                 layer_widget.deleteLater()
                 qt_layout.removeWidget(layer_widget)
                 break
 
-    def on_move_buttons_clicked(self, border_QFrame,  qt_layout, direction):
+    def on_move_buttons_clicked(self, border_QFrame,  qt_layout, direction,arch_dict):
         
-        size = len(self.architecture['layers'])
+        size = len(arch_dict['layers'])
         print(size, "Size",direction)
         for i in range(size):
             if border_QFrame == qt_layout.itemAt(i).widget():
@@ -85,8 +88,8 @@ class LayerNodeManager:
                 elif direction == 'down':
                     new_idx = i+1
 
-                layer = self.architecture['layers'].pop(i)
-                self.architecture['layers'].insert(new_idx, layer)
+                layer = arch_dict['layers'].pop(i)
+                arch_dict['layers'].insert(new_idx, layer)
 
                 qt_layout.removeWidget(layer_widget)
                 qt_layout.insertWidget(
