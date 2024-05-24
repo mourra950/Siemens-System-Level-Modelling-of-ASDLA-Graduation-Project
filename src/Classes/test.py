81cb3eaea6f943498e0916f642ccd8d961ae2da0
Omar Mohamed Yousef <mourra24@gmail.com>
2024-05-25 00:39:35 +0300

    Update test.py
    testing some class functionalities

--- src/Classes/test.py
+++ src/Classes/test.py
@@ -1,56 +1,29 @@
-from jinja2 import Template
+class Singleton(type):
+    _instances = {}
 
-# Define the data dictionary
-data = {
-    "layers": [],
-    "misc_params": {
-        "width": 1,
-        "height": 1,
-        "channels": 1,
-        "batch_size": 1,
-        "num_epochs": 1,
-        "optimizer": {
-            "type": "ASGD",
-            "params": {
-                "lr": 0.0,
-                # "lambd": 0.0,
-                # "alpha": 0.0,
-                # "t0": 0.0,
-                "weight_decay": 1,
-                "foreach": True,
-                "maximize": True,
-                "differentiable": True,
-            },
-        },
-    },
-}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(
+                Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
 
-# Define the Jinja2 template code as a string
-template_str = """
-# Static parameters
-width: {{ misc_params.width }}
-height: {{ misc_params.height }}
-channels: {{ misc_params.channels }}
-batch_size: {{ misc_params.batch_size }}
-num_epochs: {{ misc_params.num_epochs }}
 
-# Layers
-{% for layer in layers %}
-Layer: {{ layer }}
-{% endfor %}
+# Python3
+class MyClass(metaclass=Singleton):
+    def __init__(self, value):
+        self.value = value
+        self.list = []
 
-# Dynamic optimizer parameters
-optimizer_type: {{ misc_params.optimizer.type }}
-{% for param, value in misc_params.optimizer.params.items() %}
-{{ param }}: {{ value }}
-{% endfor %}
-"""
+    def add_value(self, value):
+        self.list.append(value)
 
-# Create a Jinja2 template object
-template = Template(template_str)
 
-# Render the template with the data
-result_file = template.render(layers=data["layers"], misc_params=data["misc_params"])
-
-# Output the rendered result
-print(result_file)
+t = MyClass(2)
+print(t.value)
+t1 = MyClass(3)
+print(t1.value)
+t.add_value(4)
+t.add_value(5)
+t1.add_value(6)
+t.__dict__['list']=5
+print(t.__dict__['list'])
